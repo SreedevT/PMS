579c05c11d8444a2e618f29b0cd20982356ffb47
sree <talisreedev@gmail.com>
2022-06-30 22:22:43 +0530

    better comment

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-# https://stackoverflow.com/questions/48011275/custom-user-model-fields-abstractuser-not-showing-in-django-admin
+#* For more info on inserting fields in admin page, see:
+#*  https://stackoverflow.com/questions/48011275/custom-user-model-fields-abstractuser-not-showing-in-django-admin
 fields = list(UserAdmin.fieldsets)
 fields.insert(2, ('Other Info', {'fields': ('user_type', 'gender', 'phone')}))
 UserAdmin.fieldsets = tuple(fields)
