479d2ab208ca3db2d526e66ffd1be0d68bb90e60
sree <talisreedev@gmail.com>
2022-06-30 22:23:25 +0530

    urls added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
+from django.contrib.auth import get_user_model
+
+app_name = 'accounts' #* Add app_name for namespace
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('home', views.home, name='home'),
+    path('login', auth_views.LoginView.as_view(template_name='login.html', next_page='accounts:home'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register', views.register, name='register'),
+    path('success', views.success, name='success'),
 ]
