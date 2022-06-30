b6df98fc060b7a4a029e52ca8691f1de5d4bd690
sree <talisreedev@gmail.com>
2022-06-30 22:23:35 +0530

    views added

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login
+from .forms import UserCreateForm
+from .models import User
 
-def index(request):
-    return HttpResponse('Hello, world!')
+def home(request):
+    return render(request, 'home.html', context={'user': request.user})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreateForm(request.POST)
+        print(form.is_valid())
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('success')
+    else:
+        form = UserCreateForm()
+    return render(request, 'register.html', {'form': form})
+
+def success(request):
+    return HttpResponse('Success!')
