fde06663c46061973c69580cbb77ff590bcbdd4c
sree <talisreedev@gmail.com>
2022-07-04 20:47:45 +0530

    additional context info comments addde

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login
 from .forms import UserCreateForm
+from profiles.models import PatientProfile
 
 def home(request):
-    # context = {'user': request.user} #? context may not be needed for authenticated users, see: https://stackoverflow.com/questions/13713077/get-user-information-in-django-templates
+    #* user context is passed by default to all templates, need not specify
+    #! Not needed
+    # context = {'user': request.user} #? context may not be needed for authenticated users, see: 
+    #? https://stackoverflow.com/questions/13713077/get-user-information-in-django-templates
+
+    #* since profile is a child of user (one to one relationship)
+    #* we can access the profile object by using the user object
+    #! Not needed 
+    # profile = PatientProfile.objects.get(user=request.user)
     return render(request, 'home.html')
 
 def register(request):
