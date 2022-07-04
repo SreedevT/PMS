ace009bc2ae81b71e0e2ac1a727bce2ebe0ef0ed
sree <talisreedev@gmail.com>
2022-07-04 20:48:16 +0530

    added static and media related settings

--- PMS/settings.py
+++ PMS/settings.py
@@ -15,6 +15,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'uploads'
 
 
 # Quick-start development settings - unsuitable for production
--- PMS/urls.py
+++ PMS/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
-#* For info on namespaces, see: https://docs.djangoproject.com/en/4.0/topics/http/urls/#id5
+#* For info on URL namespace, see: https://docs.djangoproject.com/en/4.0/topics/http/urls/#id5
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls', namespace='accounts')),
     path('', include('profiles.urls', namespace='profiles')),
-]
+]#+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) #?May need this for static files
+#* For info on static files, see: https://testdriven.io/blog/django-static-files/
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
