255df3b10a4ebde963320f29aa3d819c4bd105e0
sree <talisreedev@gmail.com>
2022-07-05 09:04:42 +0530

    added profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from accounts.models import User
+from .models import *
+from .forms import *
 
 def test(request):
     return HttpResponse("THIS TEST")
 
 def profile(request, pk):
-    return HttpResponse(f"This is profile {pk.profile_pic}")
+    user = User.objects.get(pk=pk)
+
+    if PatientProfile.objects.filter(user=user).exists():
+        profile = PatientProfile.objects.get(user=user)
+    else:
+        profile = PatientProfile.objects.create(user=user)
+
+    if request.method == 'POST':
+        form = PatientProfileUpdateForm(request.POST, instance=profile, files=request.FILES) #* files is used to upload files
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:home')
+    else:
+        form = PatientProfileUpdateForm(instance=profile)
+    return render(request, 'profile.html', {'form': form})
