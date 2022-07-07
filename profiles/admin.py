4a05bded91fa0eb14202eefe6dbccdab40bdeab5
Shafna31 <20it088shaf@ug.cusat.ac.in>
2022-07-07 23:09:09 +0530

    Added department and medicine model

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import PatientProfile
 from . models import DoctorProfile
+from . models import Department
+from . models import Medicine
 admin.site.register(PatientProfile)
 admin.site.register(DoctorProfile)
+admin.site.register(Department)
+admin.site.register(Medicine)
