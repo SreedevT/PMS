768910477e6dc9d0da6bd1d457d636829c6a114e
sree <talisreedev@gmail.com>
2022-07-23 22:03:51 +0530

    Locality changed

--- PMS/settings.py
+++ PMS/settings.py
@@ -122,7 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-in'
 
 TIME_ZONE = 'UTC'
 
