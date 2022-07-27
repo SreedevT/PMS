7a933a69814a0c390c0153af1fb15075740a2f96
sree <talisreedev@gmail.com>
2022-07-27 10:49:59 +0530

    widget_tweaks package added

--- PMS/settings.py
+++ PMS/settings.py
@@ -49,11 +49,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'phonenumber_field',
-    'bootstrap4',
     'profiles',
     'hospital',
     'appointment',
+    'phonenumber_field',
+    'bootstrap4',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
