d66152b199f7fa26e0e8b1298e88ebc6502d52ae
sree <talisreedev@gmail.com>
2022-07-29 23:20:29 +0530

    added doctor list page

--- appointment/views.py
+++ appointment/views.py
@@ -6,4 +6,4 @@ def doctor_list(request):
     depts = Department.objects.all()
     doctors = User.objects.filter(user_type='D')
     context = {'depts': depts, 'doctors': doctors}
-    return render(request, 'doc.html')
+    return render(request, 'doc.html', context=context)
