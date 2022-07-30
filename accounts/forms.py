5a61c5b379b05b8673ffb69344d5f9ed30b200d9
sree <talisreedev@gmail.com>
2022-07-31 00:27:34 +0530

    Removed CustomLoginForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,55 +12,7 @@ class UserCreateForm(UserCreationForm):
         #! IF NOT NEEDED BECAUSE AUTH_USER_MODEL SET IN SETTINGS.PY
 
         widgets = {
-            'user_type': forms.RadioSelect(),
-            'gender': forms.RadioSelect(),
+            'user_type': forms.RadioSelect,
+            'gender': forms.RadioSelect,
         }
 
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs) #super calls the parent class. parent callses init is called first
-    #     self.fields["username"].label = "Username" #.label is how it shows up in from as the input name ie. field name
-    #     self.fields["email"].label = "Email address"
-    #     self.fields["phone"].label = "Phone number" #? WHY
-    #     self.fields['username'].widget.attrs.update(
-    #         {'class': 'form-control', 'placeholder': 'Username'}
-    #     )
-    #     self.fields['password1'].widget.attrs.update(
-    #         {'class': 'form-control', 'placeholder': 'Password'}
-    #     )
-    #     self.fields['password2'].widget.attrs.update(
-    #         {'class': 'form-control', 'placeholder': 'Confirm Password'}
-    #     )
-    #     self.fields['first_name'].widget.attrs.update(
-    #         {'class': 'form-control', 'placeholder': 'First Name'}
-    #     )
-    #     self.fields['last_name'].widget.attrs.update(
-    #         {'class': 'form-control', 'placeholder': 'Last Name'}
-    #     )
-    #     self.fields['email'].widget.attrs.update(
-    #         {'class': 'form-control', 'placeholder': 'Email'}
-    #     )
-    #     self.fields['phone'].widget.attrs.update(
-    #         {'class': 'form-control', 'placeholder': 'Phone'}
-    #     )
-
-
-        # self.fields['user_type'].widget.attrs.update(
-        #     {'class': 'form-control', 'placeholder': 'User Type'}
-        # )
-        # self.fields['gender'].widget.attr.update(
-        #     {'class': 'form-control', 'placeholder': 'Gender'}
-        # )
-
-
-
-class CustomLoginForm(AuthenticationForm):
-
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-
-    self.fields['username'].widget.attrs.update(
-        {'class': 'form-control form-control-lg', 'placeholder': 'Username'}
-    )
-    self.fields['password'].widget.attrs.update(
-        {'class': 'form-control form-control-lg', 'placeholder': 'Password'}
-    )
