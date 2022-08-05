70f2ba7a7b64639713a11b4fef2146de5af848e2
SreedevT <72468234+SreedevT@users.noreply.github.com>
2022-08-06 03:04:20 +0530

    Updated profile page

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,14 +1,19 @@
 from django import forms
 from .models import *
 
+#? If date input type ever breaks: https://stackoverflow.com/questions/3367091/whats-the-cleanest-simplest-to-get-running-datepicker-in-django
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class PatientProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = PatientProfile
         fields = ('profile_pic', 'dob', 'blood_group', 'allergies')
 
         widgets = {
-            'dob': forms.SelectDateWidget(),
+            'dob': DateInput(),
         }
+
 
 class DoctorProfileUpdateForm(forms.ModelForm):
     class Meta:
