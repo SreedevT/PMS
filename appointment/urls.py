8edc05ed583cc32a4f48f7de3c10eca4c41b4085
SreedevT <72468234+SreedevT@users.noreply.github.com>
2022-08-06 16:52:33 +0530

    renamed few urls

--- appointment/urls.py
+++ appointment/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 app_name = 'appointment'
 urlpatterns = [
-    path('appointment', views.doctor_list, name='doc-list'),
-    path('test/', views.test, name='test'), #?TODO - remove this
-    path('book-appointment', views.book_appointment, name='book-appointment'),
-    path('booked-test', views.appointment_test, name='booked-test'),
+    path('select-doctor', views.doctor_list, name='doc-list'),
+    # path('test', views.test, name='test'), #?TODO - remove this
+    path('appointment-form', views.appointment_form, name='appointment-form'),
+    path('appointment-book', views.appointment_book, name='appointment-book'),
     path('doctor/pending-appointments', views.pending_appointments, name='pending-appointment'),
     path('doctor/view-appointment', views.view_appointment, name='view-appointment')
 ]
