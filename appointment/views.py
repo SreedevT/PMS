00892d0f8145e78dd20f291de58b7c4416d6199b
SreedevT <72468234+SreedevT@users.noreply.github.com>
2022-08-08 19:15:05 +0530

    updated with ability to create prescriptions

--- appointment/views.py
+++ appointment/views.py
@@ -1,3 +1,4 @@
+from tracemalloc import start
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -6,6 +7,7 @@ from accounts.models import User
 from .models import Appointment, Prescription
 from hospital.models import Medicine
 from django.db import connection
+from django.contrib import messages
 
 def doctor_list(request):
     context = {}
@@ -31,37 +33,42 @@ def test(request):
 def appointment_form(request):
     context = {}
 
-    if request.method == 'POST':
-        id = request.POST['id']
-        doctor = User.objects.get(id=id)
-        context = {'doctor': doctor}
-        return render(request, 'appointment-form.html', context=context)
+    if request.method != 'POST':
+        #? Display select doctor message on 'doc.html'
+        messages.add_message(request, messages.ERROR, 'Select a doctor first!')
+        return redirect('appointment:doc-list')
 
-def appointment_book(request):
+    #if POST request
+    id = request.POST['id']
+    doctor = User.objects.get(id=id)
+    context = {'doctor': doctor}
+    return render(request, 'appointment-form.html', context=context)
+
+def appointment_book(request, *args):
     #* Django model instances: https://docs.djangoproject.com/en/4.0/ref/models/instances/#django.db.models.Model.save
     context = {}
     if request.method != 'POST':
         #! Display slect doctor message on 'doc.html'
+        messages.add_message(request, messages.ERROR, 'Select a doctor first!')
         return redirect('appointment:doc-list')
-    if request.method == 'POST':
-        strslot = request.POST['slot']
-        # print(strslot)
-        list_slot = strslot.split('-')
-        # print(list_slot)
-        
-        doctor_id = request.POST['doctor']
 
-        appointment = Appointment.objects.create(
-            doctor = User.objects.get(pk=doctor_id),
-            patient = request.user,
-            date = request.POST['date'],
-            reason = request.POST['reason'],
-            start_time=list_slot[0],
-            end_time=list_slot[1],
-            status=False,
-        )
-        context={'appointment':appointment}
-        messages.success(request, 'Appointment booked successfully!')
+    # if POST request
+    strslot = request.POST['slot']
+    list_slot = strslot.split('-')
+    
+    doctor_id = request.POST['doctor']
+
+    appointment = Appointment.objects.create(
+        doctor = User.objects.get(pk=doctor_id),
+        patient = request.user,
+        date = request.POST['date'],
+        reason = request.POST['reason'],
+        start_time=list_slot[0],
+        end_time=list_slot[1],
+        status=False,
+    )
+    context={'appointment':appointment}
+    messages.success(request, 'Appointment booked successfully!')
     return render(request, 'home.html', context=context)
 
 def pending_appointments(request):
@@ -87,6 +94,26 @@ def view_appointment(request):
     context = {'appointment':appointment, 'medicines':medicines}
     return render(request, 'view-appointment.html', context=context)
 
+def follow_book(request):
+    prev_appointment = Appointment.objects.get(id=request.POST['app_id'])
+    doctor = prev_appointment.doctor
+    patient = prev_appointment.patient
+    reason = prev_appointment.reason
+    #! IF timeslots are implemented for each doctor, use doctor_id to get timeslots
+    start_time = "9:00"
+    end_time = "10:00"
+    Appointment.objects.create(
+        doctor = doctor,
+        patient = patient,
+        date = request.POST['date'],
+        reason = reason,
+        start_time = start_time,
+        end_time = end_time,
+        status = False,
+    )
+    messages.success(request, 'Appointment booked successfully!')
+    return redirect('appointment:view-appointment')
+
 def prescription(request):
     context = {}
 
@@ -107,6 +134,12 @@ def prescription(request):
         # TODO on delete of presciption, set appointment status to false
         appointment.status = True
         appointment.save(update_fields=['status']) #* SQL 'UPDATE "appointment_appointment" SET "status" = 1 WHERE "appointment_appointment"."id" = [app_id]'
-        #* Update only status fiend of appointment insted of all fields: https://docs.djangoproject.com/en/4.1/ref/models/instances/#specifying-which-fields-to-save
+        #* Update only status field of appointment insted of all fields: https://docs.djangoproject.com/en/4.1/ref/models/instances/#specifying-which-fields-to-save
         # print(connection.queries)
-    return redirect('appointment:view-appointment')
+        messages.add_message(request, messages.SUCCESS, 'Prescription added successfully!')
+
+        follow = int(request.POST['follow'])
+        if follow:
+            follow_book(request)
+
+    return redirect('appointment:pending-appointment')
