9558c9d4c3b7c41d057aa6409bc72caab871b856
SreedevT <72468234+SreedevT@users.noreply.github.com>
2022-08-16 22:49:53 +0530

    added search field in admin appointment &profile

--- appointment/admin.py
+++ appointment/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from appointment.models import Appointment, Prescription
 
-admin.site.register(Appointment)
+#* Info on search fields in admin: https://projectsplaza.com/how-to-create-search-field-in-django-admin/
+class AppointmentAdminModel(admin.ModelAdmin):
+    search_fields = ('patient__username', 'doctor__username') #* More info on use of __username: https://blndxp.wordpress.com/2017/04/11/django-amdin-related-field-got-invalid-lookup-icontains/
+
+admin.site.register(Appointment, AppointmentAdminModel)
 admin.site.register(Prescription)
--- profiles/admin.py
+++ profiles/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import *
-admin.site.register(PatientProfile)
-admin.site.register(DoctorProfile)
+
+#* Info on search fields in admin: https://projectsplaza.com/how-to-create-search-field-in-django-admin/
+class ProfileAdminModel(admin.ModelAdmin):
+    search_fields = ('user__username', ) #* More info on use of __username: https://blndxp.wordpress.com/2017/04/11/django-amdin-related-field-got-invalid-lookup-icontains/
+admin.site.register(PatientProfile, ProfileAdminModel)
+admin.site.register(DoctorProfile, ProfileAdminModel)
