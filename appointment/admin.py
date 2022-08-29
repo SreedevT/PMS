bbc492df9d4bc244b97c7d1a7dba4dc03afb641c
SreedevT <72468234+SreedevT@users.noreply.github.com>
2022-08-29 13:04:59 +0530

    commit on main

--- appointment/admin.py
+++ appointment/admin.py
@@ -4,7 +4,7 @@ from appointment.models import Appointment, Prescription, Report
 #* Info on search fields in admin: https://projectsplaza.com/how-to-create-search-field-in-django-admin/
 class AppointmentAdminModel(admin.ModelAdmin):
     #* More info on use of __username: https://blndxp.wordpress.com/2017/04/11/django-amdin-related-field-got-invalid-lookup-icontains/
-    search_fields = ('patient__username', 'doctor__username',) 
+    search_fields = ('patient__username', 'doctor__username', 'doctor__docprofile__department__name') 
     list_display = ('patient', 'doctor', 'date', 'start_time', 'status')
     list_filter = ('status', 'date',)
 
